6184fcce8c4d8e534cec37b177f429803f989643
Phani Raj <phani@planetscale.com>
2022-07-05 10:01:12 -0500

    Http Tap : save state when encountered in STDIN

--- cmd/http-tap/main.go
+++ cmd/http-tap/main.go
@@ -7,19 +7,25 @@ import (
 	"fmt"
 	"github.com/pkg/errors"
 	"github.com/planetscale/singer-tap/cmd/internal"
+	"io/fs"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"time"
 )
 
 var (
-	singerAPIURL string
-	batchSize    int
-	apiToken     string
+	singerAPIURL   string
+	batchSize      int
+	apiToken       string
+	stateDirectory string
 )
 
 func init() {
 	flag.StringVar(&singerAPIURL, "api-url", "https://api.stitchdata.com", "API Url for Singer")
-	flag.IntVar(&batchSize, "batch-size", 20, "size of each batch sent to Singer")
+	flag.IntVar(&batchSize, "batch-size", 20, "size of each batch sent to Singer, default is 20")
 	flag.StringVar(&apiToken, "api-token", "", "API Token to authenticate with Singer")
+	flag.StringVar(&stateDirectory, "state-directory", "state", "Directory to save any received state, default is state/")
 }
 
 func main() {
@@ -48,9 +54,9 @@ func execute(logger internal.Logger, apiUrl string, batchSize int, token string)
 	batchWriter := internal.NewBatchWriter(batchSize, logger, apiUrl, apiToken)
 
 	for scanner.Scan() {
-		s, r, err := parseInput(scanner.Text())
+		s, r, err := parseInput(scanner.Text(), logger)
 		if err != nil {
-			return errors.Wrap(err, "unable to identify type in STDIN")
+			return errors.Wrap(err, "unable to process output from STDIN")
 		}
 
 		if s != nil {
@@ -79,7 +85,7 @@ func execute(logger internal.Logger, apiUrl string, batchSize int, token string)
 	return batchWriter.Flush(stream)
 }
 
-func parseInput(input string) (*internal.Stream, *internal.Record, error) {
+func parseInput(input string, logger internal.Logger) (*internal.Stream, *internal.Record, error) {
 	var (
 		m MessageType
 		s internal.Stream
@@ -102,11 +108,28 @@ func parseInput(input string) (*internal.Stream, *internal.Record, error) {
 		}
 		return nil, &r, nil
 	case "STATE":
-		// do nothing
-		return nil, nil, nil
+		// save state
+		return nil, nil, saveState(logger, input, stateDirectory)
 	}
 
 	return nil, nil, fmt.Errorf("unknown message type : %v ", m.Type)
+}
+
+func saveState(logger internal.Logger, input string, path string) error {
+	now := time.Now()
+
+	statePath := filepath.Join(path, fmt.Sprintf("state-%v.json", now.UnixMilli()))
+	logger.Info(fmt.Sprintf("saving state to path : %v", statePath))
+
+	if _, err := os.OpenFile(statePath, os.O_CREATE, 0644); err != nil {
+		return errors.Wrap(err, "unable to create file to save state")
+	}
+
+	if err := ioutil.WriteFile(statePath, []byte(input), fs.ModePerm); err != nil {
+		logger.Error(fmt.Sprintf("unable to save state to path %v", statePath))
+		return errors.Wrap(err, "unable to save state")
+	}
+	return nil
 }
 
 type MessageType struct {
