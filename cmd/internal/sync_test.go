6f61d6969109293da3e388d457fba2fa69b17b8b
Phani Raj <phani@planetscale.com>
2022-07-05 12:46:47 -0500

    Add test names for sync

---
+++ cmd/internal/sync_test.go
@@ -0,0 +1,23 @@
+package internal
+
+import "testing"
+
+func TestSync_CanFilterSchema(t *testing.T) {}
+
+func TestSync_CanStartFromEmptyState(t *testing.T) {}
+
+func TestSync_PrintsStreamSchema(t *testing.T) {}
+
+func TestSync_PrintsStreamRecord(t *testing.T) {}
+
+func TestSync_PrintsStreamState(t *testing.T) {}
+
+func TestSync_UsesStateIfIncrementalSyncRequested(t *testing.T) {}
+
+func TestSync_ReturnsOldStateIfNoNewStateFound(t *testing.T) {}
+
+func TestSync_ReturnsNewStateIfFound(t *testing.T) {}
+
+func TestSync_CanPrintSelectedColumns(t *testing.T) {}
+
+func TestSync_CanPrintColumnsWithCustomFormat(t *testing.T) {}
